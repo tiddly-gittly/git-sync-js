a6ae20ec8e3d766765b2a556e0a1da317fb85bda
Lin Onetwo <linonetwo012@gmail.com>
2021-05-02 21:02:35 +0800

    feat: add extra field message in errors

--- src/errors.ts
+++ src/errors.ts
@@ -11,9 +11,12 @@ export class AssumeSyncError extends Error {
   }
 }
 export class SyncParameterMissingError extends Error {
+  /** the missing parameterName */
+  parameterName: string;
   constructor(parameterName = 'accessToken') {
     super(parameterName);
     this.name = 'SyncParameterMissingError';
+    this.parameterName = parameterName;
     this.message = `E-2 We need ${parameterName} to sync to the cloud, you should pass ${parameterName} as parameters in userInfo.`;
   }
 }
@@ -45,8 +48,11 @@ export class GitPullPushError extends Error {
 }
 
 export class CantSyncGitNotInitializedError extends Error {
+  /** the directory that should have a git repo */
+  directory: string;
   constructor(directory: string) {
     super(directory);
+    this.directory = directory;
     this.name = 'CantSyncGitNotInitializedError';
     this.message = `E-4 we can't sync on a git repository that is not initialized, maybe this folder is not a git repository. ${directory}`;
   }
@@ -61,8 +67,10 @@ export class SyncScriptIsInDeadLoopError extends Error {
 }
 
 export class CantSyncInSpecialGitStateAutoFixFailed extends Error {
+  stateMessage: string;
   constructor(stateMessage: string) {
     super(stateMessage);
+    this.stateMessage = stateMessage;
     this.name = 'CantSyncInSpecialGitStateAutoFixFailed';
     this.message = `E-6 Unable to Sync, this folder is in special condition, thus can't Sync directly. An auto-fix has been tried, but error still remains. Please resolve all the conflict manually (For example, use VSCode to open the wiki folder), if this still don't work out, please use professional Git tools (Source Tree, GitKraken) to solve this. This is caused by procedural bug in the git-sync-js.\n${stateMessage}`;
   }
