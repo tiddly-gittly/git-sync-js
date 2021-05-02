c1d8b64819fad17d8c551ac4185158ef6165fed0
Lin Onetwo <linonetwo012@gmail.com>
2021-05-02 17:27:37 +0800

    feat: add log steps and errors

--- src/errors.ts
+++ src/errors.ts
@@ -1,3 +1,6 @@
+import { truncate } from 'lodash';
+import { IGitUserInfos } from './interface';
+
 export class AssumeSyncError extends Error {
   constructor(extraMessage?: string) {
     super(extraMessage);
@@ -5,5 +8,45 @@ export class AssumeSyncError extends Error {
     this.message = `E-1 In this state, git should have been sync with the remote, but it is not, this is caused by procedural bug in the git-sync-js. ${
       extraMessage ?? ''
     }`;
+  }
+}
+export class SyncParameterMissingError extends Error {
+  constructor(parameterName: string = 'accessToken') {
+    super(parameterName);
+    this.name = 'SyncParameterMissingError';
+    this.message = `E-2 We need ${parameterName} to sync to the cloud, you should pass ${parameterName} as parameters in userInfo.`;
+  }
+}
+export class GitPullPushError extends Error {
+  constructor(
+    configuration: {
+      /** wiki folder path, can be relative */
+      dir: string;
+      /** the storage service url we are sync to, for example your github repo url */
+      remoteUrl?: string;
+      /** user info used in the commit message */
+      userInfo?: IGitUserInfos;
+    },
+    extraMessages: string,
+  ) {
+    super(extraMessages);
+    this.name = 'GitPullPushError';
+    this.message = `E-3 failed to config git to successfully pull from or push to remote with configuration ${JSON.stringify({
+      ...configuration,
+      userInfo: {
+        ...(configuration.userInfo ?? {}),
+        accessToken: truncate(configuration.userInfo?.accessToken, {
+          length: 24,
+        }),
+      },
+    })}.\n${extraMessages}`;
+  }
+}
+
+export class CantSyncGitNotInitializedError extends Error {
+  constructor() {
+    super();
+    this.name = 'CantSyncGitNotInitializedError';
+    this.message = `E-4 we can't sync on a git repository that is not initialized`;
   }
 }
