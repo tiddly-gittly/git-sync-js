c4846493355e5546c8ba75527c7507cf7cd54065
Lin Onetwo <linonetwo012@gmail.com>
2021-10-06 23:13:46 +0800

    feat: throw

--- src/index.ts
+++ src/index.ts
@@ -213,10 +213,12 @@ export async function commitAndSync(options: {
     switch (exitCode) {
       // "message":"exitCode: 128, stderr of git push: fatal: unable to access 'https://github.com/tiddly-gittly/TiddlyWiki-Chinese-Tutorial.git/': LibreSSL SSL_connect: SSL_ERROR_SYSCALL in connection to github.com:443 \n"
       case 128: {
-        // ignore network error, silent fail because user don't need TiddlyGit to tell me we don't have network connection
-        break;
+        throw new GitPullPushError(options, stderr ?? '');
       }
       // TODO: handle auth expire and throw here
+      default: {
+        throw new GitPullPushError(options, stderr ?? '');
+      }
     }
   }
 }
